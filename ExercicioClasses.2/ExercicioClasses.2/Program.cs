de6386b431bb37d259778a717b5f5a5b208ce398
KayoCosta <kaayo.costa@hotmail.com>
2022-10-04 20:18:33 -0300

    ClassExercise2

---
+++ ExercicioClasses.2/ExercicioClasses.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExercicioClasses._2;
+using System;
+using System.Globalization;
+
+namespace ExercicioClasses2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func1 = new Funcionario();
+            Funcionario func2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            func1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            func1.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture) ;
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            func2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            func2.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double media = (func1.Salario + func2.Salario) / 2.0;
+
+            Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
