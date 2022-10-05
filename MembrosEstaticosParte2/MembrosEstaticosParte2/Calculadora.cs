078cc939e2db041788be40d0f0cf9c58ec3295ce
KayoCosta <kaayo.costa@hotmail.com>
2022-10-05 00:32:41 -0300

    Static Members Part 2

---
+++ MembrosEstaticosParte2/MembrosEstaticosParte2/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿namespace MembrosEstaticosParte2
+{
+    internal class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return r * Pi * 2.0;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+
+}
